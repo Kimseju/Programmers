af68b629c15a93f9748d208b9f0a70c64b1a3c16
KIMSEJU <132471472+Kimseju@users.noreply.github.com>
2025-05-27 10:40:12 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0, a;
+        a = n / 10;
+        answer = (k*2000)+(12000*n)-(a*2000);
+        return answer;
+    }
+}
