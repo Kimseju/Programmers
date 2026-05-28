72efa4010b86482ce04c07a82425112b43374e70
KIMSEJU <132471472+Kimseju@users.noreply.github.com>
2026-05-28 23:56:09 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.11 ms, Memory: 32.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    var answer = 0;
+    if (n % 2 == 0)
+        for (let i = 0; i <= n; i+=2) answer += i * i;
+    else {
+        for (let i = 0; i <= n; i++){
+            if (i % 2 == 1) answer += i;
+        };
+    }
+    return answer;
+}
